1adb1beb9276f4f180fa49e10bb15b5de0d40e49
elshadaideshu <elshadaid48@gmail.com>
2025-01-11 03:14:08 -0800

    Add files via upload

--- library_project/library_project/deployment_setting.py
+++ library_project/library_project/deployment_setting.py
@@ -3,10 +3,10 @@ import dj_database_url
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_hosts = [os.environ.get['RENDER_EXTERNAL_HOSTNAME']] 
-CSRF_TRUSTED_ORIGINS = ['HTTPS://' +os.environ.get['RENDER_EXTERNAL_HOSTNAME']]
+ALLOWED_hosts = [os.environ.get('RENDER_EXTERNAL_HOSTNAME')] 
+CSRF_TRUSTED_ORIGINS = ['HTTPS://' +os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 DEBUG = False
-SECRET_KEY = os.environ.get['SEECRET_KEY']
+SECRET_KEY = os.environ.get('SEECRET_KEY')
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
