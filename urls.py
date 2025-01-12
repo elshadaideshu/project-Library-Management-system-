111fe5699cfc5d4d36eb24a3574aa554ad7248bd
elshadaideshu <elshadaid48@gmail.com>
2025-01-12 02:11:13 -0800

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-# library_management/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('library/', include('library.urls')),
-]
