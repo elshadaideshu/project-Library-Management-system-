0c54a8ea4436d5c38abeab4c7e294036f8230fc1
elshadaideshu <elshadaid48@gmail.com>
2025-01-12 03:41:50 -0800

    Delete library_project/library directory

--- library_project/library/urls.py
+++
@@ -1,9 +0,0 @@
-# api/urls.py
-
-from django.urls import path
-from .views import BookListCreateView, BookDetailView
-
-urlpatterns = [
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),  # For listing and creating books
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),  # For retrieving, updating, and deleting a specific book
-]
