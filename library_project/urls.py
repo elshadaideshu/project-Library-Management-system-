0210c787f42346fa0219f87ff7049b46964e7918
elshadaideshu <elshadaid48@gmail.com>
2025-01-12 02:07:57 -0800

    2nd time added but in library project folder

---
+++ library_project/urls.py
@@ -0,0 +1,9 @@
+# library_management/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('library/', include('library.urls')),
+]
