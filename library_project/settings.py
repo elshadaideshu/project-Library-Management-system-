cb7fd00b275a3846e4d5743cca04e99bb9bb0820
elshadaideshu <elshadaid48@gmail.com>
2025-01-12 04:41:38 -0800

    Add files via upload

--- library_project/settings.py
+++ library_project/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'Whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
