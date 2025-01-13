ead12169a239d1ecada09b79f8d111befe444605
elshadaideshu <elshadaid48@gmail.com>
2025-01-12 23:30:16 -0800

    Add files via upload

--- library_project/settings.py
+++ library_project/settings.py
@@ -91,6 +91,13 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFileStorage'
+STORAGES ={
+    "default":{
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles":{
+        "BACKEND":"whitenoise.storage.CompressedStaticFileStorage",
+    },
+} 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
